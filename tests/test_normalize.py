bca3b8050e5bb890e4b14960324c709f407fe1b0
mwibrow <m.wibrow@gmail.com>
2018-05-27 10:05:40 +0100

    Raise errors in class not validation function

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -4,8 +4,11 @@ Tests for the normalize module.
 
 import unittest
 
+import pandas as pd
 
-from vlnm.normalize import check_kwargs
+from vlnm.normalize import (
+    check_kwargs,
+    columns_in_dataframe)
 
 
 class TestCheckKwargs(unittest.TestCase):
@@ -15,28 +18,57 @@ class TestCheckKwargs(unittest.TestCase):
 
     def test_empty(self):
         """All empty arguments"""
-        self.assertIsNone(
-            check_kwargs('', {}))
+        required, one_of = check_kwargs({})
+        self.assertIsNone(required)
+        self.assertIsNone(one_of)
 
     def test_required_missing(self):
-        """Missing required argument raises value error"""
-        with self.assertRaises(ValueError):
-            check_kwargs('', {}, required=['missing'])
+        """Missing required argument"""
+        required, one_of = check_kwargs({}, required=['missing'])
+        self.assertIsNotNone(required)
+        self.assertIsNone(one_of)
 
     def test_required(self):
         """Required argument passes"""
-        self.assertIsNone(
-            check_kwargs('', dict(present='here'), required=['present']))
+        required, one_of = check_kwargs(
+            dict(present='here'), required=['present'])
+        self.assertIsNone(required)
+        self.assertIsNone(one_of)
 
     def test_one_of_missing(self):
-        """Missing one-of argument raises value error"""
-        with self.assertRaises(ValueError):
-            check_kwargs('', {}, one_of=[['missing']])
+        """Missing one-of argument"""
+        required, one_of = check_kwargs({}, one_of=[['missing']])
+        self.assertIsNone(required)
+        self.assertIsNotNone(one_of)
 
     def test_one_of(self):
         """One-of argument passes"""
-        self.assertIsNone(
-            check_kwargs(
-                '',
-                dict(present='here'),
-                one_of=[['present', 'and', 'correct']]))
+        required, one_of = check_kwargs(
+            dict(present='here'),
+            one_of=[['present', 'and', 'correct']])
+        self.assertIsNone(required)
+        self.assertIsNone(one_of)
+
+
+class TestColumnsInDataFrame(unittest.TestCase):
+    """
+    Test the columns_in_dataframe function
+    """
+
+    def test_empty(self):
+        """No dataframe and no columns"""
+        self.assertTrue(columns_in_dataframe(pd.DataFrame()))
+
+    def test_in(self):
+        """Columns in dataframe"""
+        self.assertTrue(
+            columns_in_dataframe(
+                pd.DataFrame(dict(a=[1, 2], b=[3, 4])),
+                ['a'], ['b']))
+
+    def test_nested(self):
+        """Nested columns in dataframe"""
+        self.assertTrue(
+            columns_in_dataframe(
+                pd.DataFrame(dict(a=[1, 2], b=[3, 4], c=[5, 6])),
+                ['a', ['b']], ['c']))
