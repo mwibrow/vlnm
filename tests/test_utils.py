5913f1addd021a2d377b862362dda102cf547fa2
mwibrow <m.wibrow@gmail.com>
2018-05-28 08:05:56 +0100

    Move functions to utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,12 @@ Test for the utils module.
 
 import unittest
 
+import pandas as pd
+
 from vlnm.utils import (
+    check_data_frame_columns,
+    check_one_from_kwargs,
+    check_required_kwargs,
     flatten,
     str_or_list)
 
@@ -64,3 +69,78 @@ class TestStrOrList(unittest.TestCase):
         value = ['value']
         actual = str_or_list(value)
         self.assertListEqual(actual, value)
+
+
+class TestDataFrameColumns(unittest.TestCase):
+    """
+    Test the check_data_frame_columns function.
+    """
+
+    def test_empty(self):
+        """No columns."""
+        self.assertIsNone(check_data_frame_columns(
+            pd.DataFrame(),
+            []))
+
+    def test_missing(self):
+        """Missing column."""
+        expected = 'column'
+        actual = check_data_frame_columns(
+            pd.DataFrame(),
+            [expected])
+        self.assertEqual(actual, expected)
+
+    def test_present(self):
+        """Column in data frame."""
+        actual = check_data_frame_columns(
+            pd.DataFrame(dict(column=[1, 2, 3])),
+            ['column'])
+        self.assertIsNone(actual)
+
+
+class TestCheckRequiredKwargs(unittest.TestCase):
+    """
+    Tests for the check_required_kwargs function.
+    """
+
+    def test_empty(self):
+        """No required keyword arguments."""
+        self.assertIsNone(check_required_kwargs({}, []))
+
+    def test_missing(self):
+        """Missing keyword arugment."""
+        expected = 'keyword'
+        actual = check_required_kwargs({}, ['keyword'])
+        self.assertEqual(actual, expected)
+
+    def test_present(self):
+        """Required keyword argument(s) present."""
+        actual = check_required_kwargs(
+            dict(keyword1=1, keyword2=2),
+            ['keyword1', 'keyword2'])
+        self.assertIsNone(actual)
+
+
+class TestCheckOneFromKwargs(unittest.TestCase):
+    """
+    Test the check_one_from_kwargs function.
+    """
+
+    def test_empty(self):
+        """No as least one-from keyword arguments."""
+        self.assertIsNone(check_one_from_kwargs({}, []))
+
+    def test_missing(self):
+        """Missing all one-from keyword arguments."""
+        one_from = [['keyword1', 'keyword2']]
+        expected = one_from
+        actual = check_one_from_kwargs({}, one_from)
+        self.assertEqual(actual, expected)
+
+    def test_present(self):
+        """At least one-from keyword argument present."""
+        one_from = [['keyword1', 'keyword2']]
+        actual = check_one_from_kwargs(
+            dict(keyword2=2),
+            one_from)
+        self.assertIsNone(actual)
--- vlnm/utils.py
+++ vlnm/utils.py
@@ -23,3 +23,35 @@ def str_or_list(value):
     if isinstance(value, list):
         return value
     return [value]
+
+def check_required_kwargs(kwargs, required):
+    """
+    Check required keyword arguments are present.
+    """
+    required = required or []
+    for key in required:
+        if not key in kwargs:
+            return key
+    return None
+
+def check_one_from_kwargs(kwargs, one_from):
+    """
+    Check at least one-from keyword arguments is present.
+    """
+    for items in one_from:
+        for item in items:
+            if item in kwargs:
+                break
+        else:
+            if one_from:
+                return one_from
+    return None
+
+def check_data_frame_columns(df, columns):
+    """
+    Check data frame contains columns.
+    """
+    for column in columns:
+        if not column in df.columns:
+            return column
+    return None
