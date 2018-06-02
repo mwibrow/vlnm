60601d1d3d15ad6b1a6d81664e6adc723976a562
Mark Wibrow <m.wibrow@gmail.com>
2018-06-02 14:43:02 +0100

    Repeat tests with random data

--- tests/test_normalizers.py
+++ tests/test_normalizers.py
@@ -24,6 +24,8 @@ from vlnm.normalizers import (
     NordstromNormalizer
 )
 
+from tests.helpers import repeat_test
+
 def generate_data_frame(
         speakers=1,
         genders=None,
@@ -90,33 +92,45 @@ class TestIntrinsicNormalizersSunnyDay(unittest.TestCase):
     """
 
     def setUp(self):
-        self.df = DATA_FRAME.copy()[['f0', 'f1', 'f2', 'f3']]
+        df = generate_data_frame(
+            speakers=8,
+            genders=['M', 'F'],
+            factors=dict(
+                group=['HV', 'LV'],
+                test=['pre', 'post'],
+                vowel=['a', 'e', 'i', 'o', 'u']))
+        self.df = df.copy()[['f0', 'f1', 'f2', 'f3']]
         self.kwargs = dict(formants=['f0', 'f1', 'f2', 'f3'])
 
+    @repeat_test()
     def test_bark_normalizer(self):
         """Test BarkNormalizer."""
         expected = hz_to_bark(self.df.copy())
         actual = BarkNormalizer().normalize(self.df, **self.kwargs)
         self.assertTrue(actual.equals(expected))
 
+    @repeat_test()
     def test_erb_normalizer(self):
         """Test ErbNormalizer."""
         expected = hz_to_erb(self.df.copy())
         actual = ErbNormalizer().normalize(self.df, **self.kwargs)
         self.assertTrue(actual.equals(expected))
 
+    @repeat_test()
     def test_log10_normalizer(self):
         """Test Log10Normalizer."""
         expected = np.log10(self.df.copy())
         actual = Log10Normalizer().normalize(self.df, **self.kwargs)
         self.assertTrue(actual.equals(expected))
 
+    @repeat_test()
     def test_log_normalizer(self):
         """Test LogNormalizer."""
         expected = np.log(self.df.copy())
         actual = LogNormalizer().normalize(self.df, **self.kwargs)
         self.assertTrue(actual.equals(expected))
 
+    @repeat_test()
     def test_mel_normalizer(self):
         """Test MelNormalizer."""
         expected = hz_to_mel(self.df.copy())
