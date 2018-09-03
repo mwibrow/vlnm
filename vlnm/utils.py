be79c3360814a00f5fb051b1b0912cddb7dfc76e
Mark Wibrow <m.wibrow@gmail.com>
2018-09-03 10:02:51 +0100

    Format list function

--- vlnm/utils.py
+++ vlnm/utils.py
@@ -2,6 +2,26 @@
 Misc. utilities.
 """
 
+def items_to_str(items, sep=',', junction=None, oxford=False):
+    """
+    Convert a list of items to a stringified list.
+    """
+    print(items, sep, junction, oxford)
+    if not items:
+        return ''
+    sorted_items = sorted(items)
+    if len(sorted_items) == 2:
+        return '{}{}{}{}'.format(
+            sorted_items[0],
+            sep if oxford else '',
+            ' {} '.format(junction) if junction else '{} '.format(sep),
+            sorted_items[1])
+    elif len(items) == 1:
+        return '{}'.format(sorted_items[0])
+    return '{}{} {}'.format(
+        sorted_items[0],
+        sep,
+        items_to_str(sorted_items[1:], sep=sep, junction=junction, oxford=oxford))
 
 def flatten(items):
     """
