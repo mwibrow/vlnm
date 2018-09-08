5fd46b67624e1666e80c269fac485f8b1ae4479b
Mark Wibrow <m.wibrow@gmail.com>
2018-09-08 11:34:23 +0100

    Start auto docstring insertion

--- vlnm/decorators.py
+++ vlnm/decorators.py
@@ -1,22 +1,35 @@
 """
 Class decorators for normalizers.
 """
+import re
 
 def docstring(cls):
     """
     Decorator for auto generating docstrings
     """
+    try:
+        column_specs = getattr(cls, '_column_specs')
+        cls.__doc__ = re.sub('{% columns %}', column_specs_docstring(column_specs), cls.__doc__)
+    except AttributeError:
+        pass
+
     return cls
 
+def column_specs_docstring(column_spec):
+    """Generate documentation from the column spec. for a class.
+    """
+    return 'Nope'
 
 
-def columns(**column_spec):
+
+def columns(**column_specs):
     """
     Decorator for specifying require and optional data frame columns.
     """
     def cls_decorator(cls):
         """
-        Decorate {cls} with {column_spec}
+        Decorate {cls} with {column_specs}
         """
-        cls._column_spec = column_spec  # pylint: disable=protected-access
+        cls._column_specs = column_specs  # pylint: disable=protected-access
+        return cls
     return cls_decorator
