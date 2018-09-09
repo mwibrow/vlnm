17178528e69b49e24140386eb2366651a80c5552
Mark Wibrow <m.wibrow@gmail.com>
2018-09-09 14:16:27 +0100

    Tests for the check columns function

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -1,153 +1,102 @@
 """
-Tests for the normalize module.
+Test for the normalize module.
 """
 
 import unittest
 
-import numpy as np
 import pandas as pd
 
-from vlnm.conversion import (
-    hz_to_bark,
-    hz_to_erb,
-    hz_to_mel)
-from vlnm.normalizers import (
-    BarkNormalizer,
-    ErbNormalizer,
-    LogNormalizer,
-    Log10Normalizer,
-    MelNormalizer)
+from vlnm.normalize import (
+    check_columns,
+    update_options
+)
 
-from tests.helpers import (
-    generate_data_frame,
-    repeat_test)
-
-
-def get_test_dataframe(speakers=8):
-    """Generate a test dataframe."""
-    df = generate_data_frame(
-        speakers=speakers,
-        genders=['M', 'F'],
-        factors=dict(
-            group=['HV', 'LV'],
-            test=['pre', 'post'],
-            vowel=['a', 'e', 'i', 'o', 'u']))
-    return df
-
-
-DATA_FRAME = get_test_dataframe()
-
-class TestIntrinsicNormalizersSunnyDay(unittest.TestCase):
+class TestCheckColumns(unittest.TestCase):
     """
-    Sunny day tests for normalizers
+    Tests for the check_columns function.
     """
 
     def setUp(self):
-        df = generate_data_frame(
-            speakers=8,
-            genders=['M', 'F'],
-            factors=dict(
-                group=['HV', 'LV'],
-                test=['pre', 'post'],
-                vowel=['a', 'e', 'i', 'o', 'u']))
-        self.df = df.copy()[['f0', 'f1', 'f2', 'f3']]
-        self.kwargs = dict(formants=['f0', 'f1', 'f2', 'f3'])
+        self.df = pd.DataFrame(dict(
+            speaker=['speaker1', 'speaker2', 'speaker3'],
+            gender='gender',
+            f0=[50, 100, 150],
+            f1=[350, 400, 450],
+            f2=[750, 800, 850]))
 
-    @repeat_test()
-    def test_bark_normalizer(self):
-        """Test BarkNormalizer."""
-        expected = hz_to_bark(self.df.copy())
-        actual = BarkNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
+    def test_required_column_no_alias_error(self):
+        """
+        Error if required column not in data frame.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=['vowel']),
+                {},
+                [])
 
-    @repeat_test()
-    def test_erb_normalizer(self):
-        """Test ErbNormalizer."""
-        expected = hz_to_erb(self.df.copy())
-        actual = ErbNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
+    def test_required_aliased_column_error(self):
+        """
+        Error if required alised column not in data frame.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=['vowel']),
+                dict(vowel='word'),
+                [])
 
-    @repeat_test()
-    def test_log10_normalizer(self):
-        """Test Log10Normalizer."""
-        expected = np.log10(self.df.copy())
-        actual = Log10Normalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
+    def test_choice_columns_error(self):
+        """
+        Error if one of a choice column is not in data frame.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=[], formants=['f3']),
+                {},
+                [])
 
-    @repeat_test()
-    def test_log_normalizer(self):
-        """Test LogNormalizer."""
-        expected = np.log(self.df.copy())
-        actual = LogNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
+    def test_choice_column_aliased_error(self):
+        """
+        Error if an aliased choicecolumn is not in data frame.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=[], formants=['f3']),
+                dict(f3='f3@50'),
+                [])
 
-    @repeat_test()
-    def test_mel_normalizer(self):
-        """Test MelNormalizer."""
-        expected = hz_to_mel(self.df.copy())
-        actual = MelNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
+    def test_formant_column_error(self):
+        """
+        Error if an invalid formant is given.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=[], formants=['fk']),
+                {},
+                [])
 
+    def test_group_column_error(self):
+        """
+        Error if a grouping column is not in the data frame.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=[]),
+                {},
+                ['test'])
 
-
-def make_new_columns_expected(df, new_columns, transform):
-    """Helper for class Test new columns."""
-    tmp_df = transform(df.copy())
-    tmp_df.columns = [new_columns.format(column) for column in tmp_df.columns]
-    return pd.concat([df.copy(), tmp_df], axis=1)
-
-class TestIntrinsicNormalizersNewColumns(unittest.TestCase):
-    """
-    Test normalized formants as new columns.
-    """
-
-    def setUp(self):
-        self.new_columns = '{}_N'
-        df = generate_data_frame(
-            speakers=8,
-            genders=['M', 'F'],
-            factors=dict(
-                group=['HV', 'LV'],
-                test=['pre', 'post'],
-                vowel=['a', 'e', 'i', 'o', 'u']))
-        self.df = df.copy()[['f0', 'f1', 'f2', 'f3']]
-        self.kwargs = dict(
-            formants=['f0', 'f1', 'f2', 'f3'],
-            new_columns=self.new_columns)
-
-    @repeat_test()
-    def test_bark_normalizer(self):
-        """Test BarkNormalizer."""
-        expected = make_new_columns_expected(self.df, self.new_columns, hz_to_bark)
-        actual = BarkNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
-
-    @repeat_test()
-    def test_erb_normalizer(self):
-        """Test ErbNormalizer."""
-        expected = make_new_columns_expected(self.df, self.new_columns, hz_to_erb)
-        actual = ErbNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
-
-    @repeat_test()
-    def test_log10_normalizer(self):
-        """Test Log10Normalizer."""
-        expected = make_new_columns_expected(self.df, self.new_columns, np.log10)
-        actual = Log10Normalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
-
-    @repeat_test()
-    def test_log_normalizer(self):
-        """Test LogNormalizer."""
-        expected = make_new_columns_expected(self.df, self.new_columns, np.log)
-        actual = LogNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
-
-    @repeat_test()
-    def test_mel_normalizer(self):
-        """Test MelNormalizer."""
-        expected = make_new_columns_expected(self.df, self.new_columns, hz_to_mel)
-        actual = MelNormalizer().normalize(self.df, **self.kwargs)
-        self.assertTrue(actual.equals(expected))
-
-
+    def test_group_column_aliased_error(self):
+        """
+        Error if an aliased grouping column is not in the data frame.
+        """
+        with self.assertRaises(ValueError):
+            check_columns(
+                self.df,
+                dict(required=[]),
+                dict(test='other'),
+                ['test'])
---
+++ tests/test_normalizers_old.py
@@ -0,0 +1,595 @@
+"""
+Tests for the normalizers library.
+"""
+
+import unittest
+
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from vlnm.conversion import (
+    hz_to_bark,
+    hz_to_erb,
+    hz_to_mel
+)
+
+from vlnm.normalizers import (
+    BarkNormalizer,
+    BarkDifferenceNormalizer,
+    BladenNormalizer,
+    ErbNormalizer,
+    GerstmanNormalizer,
+    LobanovNormalizer,
+    Log10Normalizer,
+    LogNormalizer,
+    LCENormalizer,
+    MelNormalizer,
+    NearyNormalizer,
+    NordstromNormalizer
+)
+
+from tests.helpers import (
+    generate_data_frame,
+    repeat_test)
+
+
+def get_test_dataframe(speakers=8):
+    """Generate a test dataframe."""
+    df = generate_data_frame(
+        speakers=speakers,
+        genders=['M', 'F'],
+        factors=dict(
+            group=['HV', 'LV'],
+            test=['pre', 'post'],
+            vowel=['a', 'e', 'i', 'o', 'u']))
+    return df
+
+
+DATA_FRAME = get_test_dataframe()
+
+class TestIntrinsicNormalizersValueError(unittest.TestCase):
+    """
+    Check normalizers raise ValueError if no formants specified.
+    """
+
+    def test_bark_normalizer(self):
+        """Test BarkNormalizer."""
+        with self.assertRaises(ValueError):
+            BarkNormalizer().normalize(DATA_FRAME)
+
+    def test_erb_normalizer(self):
+        """Test ErbNormalizer."""
+        with self.assertRaises(ValueError):
+            ErbNormalizer().normalize(DATA_FRAME)
+
+    def test_log10_normalizer(self):
+        """Test Log10Normalizer."""
+        with self.assertRaises(ValueError):
+            Log10Normalizer().normalize(DATA_FRAME)
+
+    def test_log_normalizer(self):
+        """Test LogNormalizer."""
+        with self.assertRaises(ValueError):
+            LogNormalizer().normalize(DATA_FRAME)
+
+    def test_mel_normalizer(self):
+        """Test MelNormalizer."""
+        with self.assertRaises(ValueError):
+            MelNormalizer().normalize(DATA_FRAME)
+
+
+class TestIntrinsicNormalizersSunnyDay(unittest.TestCase):
+    """
+    Sunny day tests for normalizers
+    """
+
+    def setUp(self):
+        df = generate_data_frame(
+            speakers=8,
+            genders=['M', 'F'],
+            factors=dict(
+                group=['HV', 'LV'],
+                test=['pre', 'post'],
+                vowel=['a', 'e', 'i', 'o', 'u']))
+        self.df = df.copy()[['f0', 'f1', 'f2', 'f3']]
+        self.kwargs = dict(formants=['f0', 'f1', 'f2', 'f3'])
+
+    @repeat_test()
+    def test_bark_normalizer(self):
+        """Test BarkNormalizer."""
+        expected = hz_to_bark(self.df.copy())
+        actual = BarkNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_erb_normalizer(self):
+        """Test ErbNormalizer."""
+        expected = hz_to_erb(self.df.copy())
+        actual = ErbNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_log10_normalizer(self):
+        """Test Log10Normalizer."""
+        expected = np.log10(self.df.copy())
+        actual = Log10Normalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_log_normalizer(self):
+        """Test LogNormalizer."""
+        expected = np.log(self.df.copy())
+        actual = LogNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_mel_normalizer(self):
+        """Test MelNormalizer."""
+        expected = hz_to_mel(self.df.copy())
+        actual = MelNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+
+
+def make_new_columns_expected(df, suffix, transform):
+    """Helper for class Test new columns."""
+    tmp_df = transform(df.copy())
+    tmp_df.columns = ['{}{}'.format(column, suffix) for column in tmp_df.columns]
+    return pd.concat([df.copy(), tmp_df], axis=1)
+
+class TestIntrinsicNormalizersNewColumns(unittest.TestCase):
+    """
+    Test normalized formants as new columns.
+    """
+
+    def setUp(self):
+        self.suffix = '_N'
+        df = generate_data_frame(
+            speakers=8,
+            genders=['M', 'F'],
+            factors=dict(
+                group=['HV', 'LV'],
+                test=['pre', 'post'],
+                vowel=['a', 'e', 'i', 'o', 'u']))
+        self.df = df.copy()[['f0', 'f1', 'f2', 'f3']]
+        self.kwargs = dict(
+            formants=['f0', 'f1', 'f2', 'f3'],
+            suffix=self.suffix)
+
+    @repeat_test()
+    def test_bark_normalizer(self):
+        """Test BarkNormalizer."""
+        expected = make_new_columns_expected(self.df, self.suffix, hz_to_bark)
+        actual = BarkNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_erb_normalizer(self):
+        """Test ErbNormalizer."""
+        expected = make_new_columns_expected(self.df, self.suffix, hz_to_erb)
+        actual = ErbNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_log10_normalizer(self):
+        """Test Log10Normalizer."""
+        expected = make_new_columns_expected(self.df, self.suffix, np.log10)
+        actual = Log10Normalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_log_normalizer(self):
+        """Test LogNormalizer."""
+        expected = make_new_columns_expected(self.df, self.suffix, np.log)
+        actual = LogNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+    @repeat_test()
+    def test_mel_normalizer(self):
+        """Test MelNormalizer."""
+        expected = make_new_columns_expected(self.df, self.suffix, hz_to_mel)
+        actual = MelNormalizer().normalize(self.df, **self.kwargs)
+        self.assertTrue(actual.equals(expected))
+
+
+class TestBladenNormalizer(unittest.TestCase):
+    """
+    Test the BladenNormalizer.
+    """
+
+    def setUp(self):
+        self.df = DATA_FRAME.copy()
+        self.formants = ['f0', 'f1', 'f2', 'f3']
+        self.kwargs = dict(
+            formants=self.formants)
+
+    def test_no_gender(self):
+        """No gender column raises ValueError."""
+        with self.assertRaises(ValueError):
+            BladenNormalizer().normalize(self.df, **self.kwargs)
+
+    def test_no_male_or_female(self):
+        """No female or male column raises ValueError."""
+        with self.assertRaises(ValueError):
+            BladenNormalizer().normalize(self.df, gender='gender', **self.kwargs)
+
+    def test_minimal_kwargs(self):
+        """Minimally correct kwargs."""
+        BladenNormalizer().normalize(
+            self.df,
+            gender='gender', male='M', **self.kwargs)
+
+    def test_with_female(self):
+        """Test specifying female label."""
+        normalizer = BladenNormalizer()
+
+        expected = hz_to_bark(self.df.copy()[self.formants])
+        expected[self.df['gender'] == 'F'] -= 1.
+        actual = normalizer.normalize(
+            self.df,
+            gender='gender',
+            female='F',
+            **self.kwargs)[self.formants]
+        self.assertTrue(actual.equals(expected))
+
+    def test_with_male(self):
+        """Test specifying male label."""
+        normalizer = BladenNormalizer()
+        expected = hz_to_bark(self.df.copy()[self.formants])
+        expected[self.df['gender'] == 'F'] -= 1.
+        actual = normalizer.normalize(
+            self.df,
+            gender='gender',
+            male='M',
+            **self.kwargs)[self.formants]
+        self.assertTrue(actual.equals(expected))
+
+
+class TestBarkDifferenceNormalizer(unittest.TestCase):
+    """
+    Test the BarkDifferenceNormalizer class
+    """
+
+    def setUp(self):
+        self.df = DATA_FRAME.copy()
+        self.formants = ['f2', 'f3']
+        self.kwargs = dict(formants=self.formants)
+
+    def test_no_f0_or_f1(self):
+        """No f0 or f1 columns ValueError."""
+        with self.assertRaises(ValueError):
+            BarkDifferenceNormalizer().normalize(self.df, **self.kwargs)
+
+    def test_f0(self):
+        """F0 subtraction."""
+        expected = self.df.copy()
+        for formant in self.formants:
+            expected[formant] = (hz_to_bark(self.df[formant]) -
+                                 hz_to_bark(self.df['f0']))
+        actual = BarkDifferenceNormalizer().normalize(
+            self.df, f0='f0', **self.kwargs)
+        self.assertTrue(actual[self.formants].equals(expected[self.formants]))
+
+    def test_f1(self):
+        """F1 subtraction."""
+        expected = self.df.copy()
+        for formant in self.formants:
+            expected[formant] = (hz_to_bark(self.df[formant]) -
+                                 hz_to_bark(self.df['f1']))
+        actual = BarkDifferenceNormalizer().normalize(
+            self.df, f1='f1', **self.kwargs)
+        self.assertTrue(actual[self.formants].equals(expected[self.formants]))
+
+
+class TestNordstromNormalizer(unittest.TestCase):
+    """
+    Tests for the NordstromNormalizer class.
+    """
+
+    def setUp(self):
+        self.df = DATA_FRAME.copy()
+        self.formants = ['f2', 'f3']
+        self.kwargs = dict(
+            formants=self.formants)
+
+    def test_no_f1_or_f3(self):
+        """No f1 or f3 columns ValueError."""
+        with self.assertRaises(ValueError):
+            NordstromNormalizer().normalize(self.df, formants=[])
+
+    def test_mu_ratio(self):
+        """Calculate mu ratios."""
+        df = self.df
+        mu_male = df[(df['gender'] == 'M') & (df['f1'] > 600)]['f3'].mean()
+        mu_female = df[(df['gender'] == 'F') & (df['f1'] > 600)]['f3'].mean()
+
+        constants = {}
+        NordstromNormalizer().calculate_f3_means(
+            df,
+            f1='f1',
+            f3='f3',
+            constants=constants,
+            gender='gender',
+            female='F')
+        self.assertEqual(constants['mu_female'], mu_female)
+        self.assertEqual(constants['mu_male'], mu_male)
+
+
+class TestLCENormalizer(unittest.TestCase):
+    """
+    Tests for the LCENormalizer class.
+    """
+
+    def setUp(self):
+        self.df = get_test_dataframe()
+        self.kwargs = dict(
+            formants=['f1', 'f2', 'f3'])
+
+    @repeat_test()
+    def test_speaker_summary(self):
+        """Check maximum formant value for all speakers."""
+        for speaker in self.df['speaker'].unique():
+            df = self.df[self.df['speaker'] == speaker]
+            cols_in = self.kwargs['formants']
+            expected = {}
+            for col in cols_in:
+                expected['{}_max'.format(col)] = df[col].max()
+            actual = {}
+            LCENormalizer().speaker_summary(
+                df,
+                cols_in=self.kwargs['formants'],
+                constants=actual)
+            self.assertDictEqual(actual, expected)
+
+    @repeat_test()
+    def test_output(self):
+        """
+        Check normalized formant output.
+        """
+        suffix = '_N'
+        df = self.df.copy()
+        formants = self.kwargs['formants']
+        expected = df.groupby('speaker', as_index=False).apply(
+            lambda x: lce_helper(x, formants, suffix))
+
+        actual = LCENormalizer().normalize(
+            self.df,
+            formants=[formants],
+            suffix=suffix,
+            speaker='speaker')
+
+        expected = expected.dropna().sort_values(
+            by=sorted(expected.columns)).reset_index(drop=True)
+        actual = actual.dropna().sort_values(
+            by=sorted(actual.columns)).reset_index(drop=True)
+
+        try:
+            assert_frame_equal(
+                actual,
+                expected,
+                check_exact=False,
+                check_less_precise=False)
+        except AssertionError:
+            for i in range(len(expected)):
+                self.assertDictEqual(
+                    actual.loc[i, :].to_dict(),
+                    expected.loc[i, :].to_dict())
+
+
+def lce_helper(df, formants, suffix):
+    """Helper for LCENormalizerTests."""
+    in_cols = formants
+    out_cols = ['{}{}'.format(col, suffix) for col in in_cols]
+    f_max = df[in_cols].max()
+    df[out_cols] = df[in_cols] / f_max
+    return df
+
+
+class TestGerstmanNormalizer(unittest.TestCase):
+    """
+    Tests for the GerstmanNormalizer class.
+    """
+
+    def setUp(self):
+        self.df = get_test_dataframe()
+        self.kwargs = dict(
+            formants=['f1', 'f2', 'f3'])
+
+    def test_no_formants(self):
+        """No formant column raises value error."""
+        with self.assertRaises(ValueError):
+            GerstmanNormalizer().normalize(self.df)
+
+    @repeat_test()
+    def test_speaker_summary(self):
+        """Check maximum and minium formant value for all speakers."""
+        for speaker in self.df['speaker'].unique():
+            df = self.df[self.df['speaker'] == speaker]
+            cols_in = self.kwargs['formants']
+            expected = {}
+            for col in cols_in:
+                expected['{}_max'.format(col)] = df[col].max()
+                expected['{}_min'.format(col)] = df[col].min()
+            actual = {}
+            GerstmanNormalizer().speaker_summary(
+                df,
+                cols_in=self.kwargs['formants'],
+                constants=actual)
+            self.assertDictEqual(actual, expected)
+
+    @repeat_test()
+    def test_output(self):
+        """
+        Check normalized formant output.
+        """
+        suffix = '_N'
+        df = self.df.copy()
+        formants = self.kwargs['formants']
+        expected = df.groupby('speaker', as_index=False).apply(
+            lambda x: gerstman_helper(x, formants, suffix))
+
+        actual = GerstmanNormalizer().normalize(
+            self.df,
+            formants=[formants],
+            suffix=suffix,
+            speaker='speaker')
+
+        expected = expected.dropna().sort_values(
+            by=sorted(expected.columns)).reset_index(drop=True)
+        actual = actual.dropna().sort_values(
+            by=sorted(actual.columns)).reset_index(drop=True)
+
+        try:
+            assert_frame_equal(
+                actual,
+                expected,
+                check_exact=False,
+                check_less_precise=False)
+        except AssertionError:
+            for i in range(len(expected)):
+                self.assertDictEqual(
+                    actual.loc[i, :].to_dict(),
+                    expected.loc[i, :].to_dict())
+
+
+def gerstman_helper(df, formants, suffix):
+    """Helper for GerstmanNormalizerTests."""
+    in_cols = formants
+    out_cols = ['{}{}'.format(col, suffix) for col in in_cols]
+    f_min = df[in_cols].min()
+    f_max = df[in_cols].max()
+    df[out_cols] = 999 * (df[in_cols] - f_min) / (f_max - f_min)
+    return df
+
+
+class TestLobanovNormalizer(unittest.TestCase):
+    """
+    Tests for the LobanovNormalizer class.
+    """
+
+    def setUp(self):
+        self.df = get_test_dataframe()
+        self.kwargs = dict(
+            formants=['f1', 'f2', 'f3'])
+
+    def test_no_formants(self):
+        """No formant column raises value error."""
+        with self.assertRaises(ValueError):
+            LobanovNormalizer().normalize(self.df)
+
+    @repeat_test()
+    def test_speaker_summary(self):
+        """Check mean and standard formant values for all speakers."""
+        for speaker in self.df['speaker'].unique():
+            df = self.df[self.df['speaker'] == speaker]
+            cols_in = self.kwargs['formants']
+            expected = {}
+            for col in cols_in:
+                expected['{}_mu'.format(col)] = df[col].mean()
+                expected['{}_sigma'.format(col)] = df[col].std()
+            actual = {}
+            LobanovNormalizer().speaker_summary(
+                df,
+                cols_in=self.kwargs['formants'],
+                constants=actual)
+            self.assertDictEqual(actual, expected)
+
+    @repeat_test()
+    def test_output(self):
+        """
+        Check normalized formant output.
+        """
+        suffix = '_N'
+        df = self.df.copy()
+        formants = self.kwargs['formants']
+        expected = df.groupby('speaker', as_index=False).apply(
+            lambda x: lobanov_helper(x, formants, suffix))
+
+        actual = LobanovNormalizer().normalize(
+            self.df,
+            formants=[formants],
+            suffix=suffix,
+            speaker='speaker')
+
+        expected = expected.dropna().sort_values(
+            by=sorted(expected.columns)).reset_index(drop=True)
+        actual = actual.dropna().sort_values(
+            by=sorted(actual.columns)).reset_index(drop=True)
+
+        try:
+            assert_frame_equal(
+                actual,
+                expected,
+                check_exact=False,
+                check_less_precise=False)
+        except AssertionError:
+            for i in range(len(expected)):
+                self.assertDictEqual(
+                    actual.loc[i, :].to_dict(),
+                    expected.loc[i, :].to_dict())
+
+
+def lobanov_helper(df, formants, suffix):
+    """Helper for LobanovNormalizerTests."""
+    in_cols = formants
+    out_cols = ['{}{}'.format(col, suffix) for col in in_cols]
+    f_mu = df[formants].mean()
+    f_sigma = df[formants].std()
+    df[out_cols] = (df[in_cols] - f_mu) / f_sigma
+    return df
+
+
+class TestNearyNormalizer(unittest.TestCase):
+    """
+    Tests for the NearyNormalizer class.
+    """
+
+    def setUp(self):
+        self.df = get_test_dataframe()
+        self.kwargs = dict(
+            formants=['f1', 'f2', 'f3'])
+
+    def test_no_formants(self):
+        """No formant column raises value error."""
+        with self.assertRaises(ValueError):
+            NearyNormalizer().normalize(self.df)
+
+    @repeat_test()
+    def test_speaker_summary(self):
+        """
+        Check formant intrinsic formant values for all speakers.
+        """
+        for speaker in self.df['speaker'].unique():
+            df = self.df[self.df['speaker'] == speaker]
+            cols_in = self.kwargs['formants']
+            expected = {}
+            for col in cols_in:
+                expected['{}_mu_log'.format(col)] = (
+                    np.mean(np.log(df[col].dropna())))
+            actual = {}
+            NearyNormalizer().speaker_summary(
+                df,
+                cols_in=self.kwargs['formants'],
+                constants=actual)
+            self.assertDictEqual(actual, expected)
+
+    @repeat_test()
+    def test_speaker_summary_extrinsic(self):
+        """
+        Check formant extrinsic formant values for all speakers.
+        """
+        for speaker in self.df['speaker'].unique():
+            df = self.df[self.df['speaker'] == speaker]
+            cols_in = self.kwargs['formants']
+            expected = {}
+            mu_log = np.mean(np.mean(np.log(df[cols_in].dropna())))
+            for col in cols_in:
+                expected['{}_mu_log'.format(col)] = mu_log
+            actual = {}
+            NearyNormalizer().speaker_summary(
+                df,
+                cols_in=self.kwargs['formants'],
+                constants=actual,
+                method='extrinsic')
+            self.assertDictEqual(actual, expected)
