2cef1a9c66dfa52499df7f12cd1825528fa5f37a
mwibrow <m.wibrow@gmail.com>
2018-09-16 10:46:10 +0100

    100% coverage of utils

--- vlnm/utils.py
+++ vlnm/utils.py
@@ -50,16 +50,6 @@ def merge_columns(column_specs, kwargs):
                     kwargs[item] = item
     return kwargs
 
-REQUIRED_COLUMN_NOT_FOUND = (
-    'Required column \'{}\' not found in the data frame')
-REQUIRED_COLUMN_GIVEN_NOT_FOUND = (
-    'Required {} column (given as \'{}\') missing in data frame')
-COLUMN_GIVEN_NOT_FOUND = (
-    '{} column (given as \'{}\') missing in data frame')
-AT_LEAST_ONE_COLUMN_NOT_FOUND = (
-    'Expected at least one column from \'{}\' to be in data frame')
-COLUMN_NOT_FOUND = (
-    'Column \'{}\' missing in data frame')
 
 def flatten(items):
     """
@@ -81,35 +71,3 @@ def str_or_list(value):
     if isinstance(value, list):
         return value
     return [value]
-
-# def check_required_kwargs(kwargs, required):
-#     """
-#     Check required keyword arguments are present.
-#     """
-#     required = required or []
-#     for key in required:
-#         if not key in kwargs:
-#             return key
-#     return None
-
-def check_one_from_kwargs(kwargs, one_from):
-    """
-    Check at least one-from keyword arguments is present.
-    """
-    for items in one_from:
-        for item in items:
-            if item in kwargs:
-                break
-        else:
-            if one_from:
-                return one_from
-    return None
-
-def check_data_frame_columns(df, columns):
-    """
-    Check data frame contains columns.
-    """
-    for column in columns:
-        if not column in df.columns:
-            return column
-    return None
