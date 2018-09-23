d812629101aed2610ca237e5466272716bd0345a
Mark Wibrow <m.wibrow@gmail.com>
2018-09-23 11:13:55 +0100

    Add module docs

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,9 @@
 """
 Placeholder required for pylint.
+
+See https://github.com/PyCQA/pylint/issues/352
+
+NB This repo uses an older version of pylint.
+For detauls see https://github.com/PyCQA/pylint/issues/2198
+
 """
