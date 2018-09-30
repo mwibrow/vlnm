8d92b1dbd6de6b254103919401ae08ce4d41bdc6
Mark Wibrow <m.wibrow@gmail.com>
2018-09-30 08:32:54 +0100

    Update module 'exports'

---
+++ vlnm/normalizers/__init__.py
@@ -0,0 +1,24 @@
+"""
+Export normaliszers
+"""
+
+from vlnm.normalizers import (
+    BarkDifferenceNormalizer,
+    BarkNormalizer,
+    BighamNormalizer,
+    BladenNormalizer,
+    ErbNormalizer,
+    GerstmanNormalizer,
+    LCENormalizer,
+    LobanovNormalizer,
+    Log10Normalizer,
+    LogNormalizer,
+    MelNormalizer,
+    NearyGMNormalizer,
+    NearyNormalizer,
+    NordstromNormalizer,
+    SchwaNormalizer,
+    VowelNormalizer,
+    WattFabricius2Normalizer,
+    WattFabricius3Normalizer,
+    WattFabriciusNormalizer)
