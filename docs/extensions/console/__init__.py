41c7984bf2cb45d7baeb3a6fce62dc08ae075983
Mark Wibrow <m.wibrow@gmail.com>
2018-10-31 08:37:58 +0000

    Iterate over statements before generating output

--- docs/extensions/console/__init__.py
+++ docs/extensions/console/__init__.py
@@ -3,7 +3,7 @@ Console directive.
 """
 
 from code import compile_command, InteractiveInterpreter
-from contextlib import redirect_stdout
+from contextlib import redirect_stdout, redirect_stderr
 from io import StringIO
 import os
 import re
@@ -20,67 +20,77 @@ class ConsoleDirective(CodeBlock):
 
     def run(self):
         """Run directive"""
-        env = self.state.document.settings.env
+        self.arguments = ['python']
 
+        env = self.state.document.settings.env
         local_env = dict(
             __file__=os.path.normpath(env.relfn2path('.')[1]),
             __doc__=None,
             __name__='__console__')
-
-        self.arguments = ['python']
-        console = []
-        code = ''
-        code_objects = []
-        code_object = None
-        initial_prefix = '>>> '
-        continuation_prefix = '... '
-        magic_prefix = '###'
-
-        statement = initial_prefix
         interpreter = InteractiveInterpreter(locals=local_env)
 
-        hidden = False
-
         items = [item for item in self.content]
-        for item in items:
-            if item.startswith(magic_prefix):
-                hidden = not hidden
-                continue
-            line = re.sub(r'^[>.]{3}\s?', '', item)
-            code += line
-            if code_object:  # from previous iteration.
-                if line:
-                    statement = initial_prefix + line
-            else:
-                statement += line
-            try:
-                code_object = compile_command(code)
-                if code_object is None:
-                    code += '\n'
-                    statement += '\n' + continuation_prefix
-                else:
-                    code = ''
-                    if statement and not hidden:
-                        console.append(statement)
-                    stdout = StringIO()
-                    with redirect_stdout(stdout):
-                        interpreter.runcode(code_object)
-                    output = stdout.getvalue()
-                    stdout.close()
-                    if output and not hidden:
-                        console.append(output[:-1])
-                    statement = ''
-                    code_objects.append(code_object)
-
-            except SyntaxError:
-                code += r'\n'
-                statement += r'\n'
-                code_object = None
+        console = []
+        for statement, magic, code_object in generate_statements(items):
+            if not magic:
+                console.append(statement)
+            stdout = StringIO()
+            stderr = StringIO()
+            with redirect_stdout(stdout), redirect_stderr(stderr):
+                interpreter.runcode(code_object)
+            stdout_output = stdout.getvalue()
+            stderr_output = stderr.getvalue()
+            stdout.close()
+            stderr.close()
+            if stdout_output and not magic:
+                console.append(stdout_output[:-1])
+            if stderr_output:
+                console.append(stderr_output[:-1])
 
         self.content = console
         nodes = super(ConsoleDirective, self).run()
-        nodes[0].attributes['classes'] = ['console']
-        return nodes
+        parent = docutils.nodes.line_block(classes=['console'])
+        parent += nodes[0]
+        return [parent]
+
+def generate_statements(content):
+    """Generator for statements and code_objects.
+    """
+    hidden = False
+    initial_prefix = '>>> '
+    continuation_prefix = '... '
+    magic_prefix = '###'
+    magic = ''
+    code_object = None
+    code = ''
+    statement = initial_prefix
+    items = [item for item in content]
+    for item in items:
+        if item.startswith(magic_prefix):
+            hidden = not hidden
+            magic = 'hidden' if hidden else ''
+            continue
+        line = re.sub(r'^[>.]{3}\s?', '', item)
+        code += line
+        if code_object:  # from previous iteration.
+            if line:
+                statement = initial_prefix + line
+        else:
+            statement += line
+        try:
+            code_object = compile_command(code)
+            if code_object is None:
+                code += '\n'
+                statement += '\n' + continuation_prefix
+            else:
+                code = ''
+                if statement:
+                    yield (statement, magic, code_object)
+                statement = ''
+        except SyntaxError:
+            code += r'\n'
+            statement += r'\n'
+            code_object = None
 
 def setup(app):
     """
