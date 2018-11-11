cc39d90f26e7b3ecfa3beedce2935df698bde5f5
Mark Wibrow <m.wibrow@gmail.com>
2018-11-11 13:55:39 +0000

    Reintroduce test base module

---
+++ tests/test_base.py
@@ -0,0 +1,11 @@
+"""
+Tests for the base module
+"""
+
+from tests.helpers import Helper
+
+
+class TestNormalizer(Helper.TestNormalizerBase):
+    """
+    Tests for the base Normalizer class.
+    """
