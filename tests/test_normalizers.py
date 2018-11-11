6f35d522bc3fb4525b20d4b09cbe28c1b59c7dbe
Mark Wibrow <m.wibrow@gmail.com>
2018-11-11 14:21:24 +0000

    Add tests for normalizer pacakge

---
+++ tests/test_normalizers.py
@@ -0,0 +1,80 @@
+"""
+Tests for the normalizer package.
+"""
+
+import unittest
+
+from vlnm.normalizers import (
+    get_normalizer,
+    list_normalizers,
+    register_normalizer
+)
+
+class TestGetNormalizer(unittest.TestCase):
+    """
+    Tests for the get_normalizer function.
+    """
+
+    def test_empty_method(self):
+        """
+        Empty string raises ValueError.
+        """
+        with self.assertRaises(ValueError):
+            get_normalizer('')
+
+    def test_unknown_method(self):
+        """
+        Invalid method raises NameError.
+        """
+        with self.assertRaises(NameError):
+            get_normalizer('test_normalizer')
+
+    def test_ambiguous_method(self):
+        """
+        Ambiguous method fraises NameError.
+        """
+        register = dict(
+            test_normalizer1=True,
+            test_normalizer2=False)
+        with self.assertRaises(NameError):
+            get_normalizer('test_normalizer', register=register)
+
+    def test_retrieve_normalizer(self):
+        """
+        Return value in register.
+        """
+        register = dict(
+            test_normalizer1=True,
+            test_normalizer2=False)
+        expected = True
+        actual = get_normalizer('test_normalizer1', register=register)
+        self.assertEqual(actual, expected)
+
+
+class TestListNormalizers(unittest.TestCase):
+    """
+    Tests for the list_normalizers function.
+    """
+
+    def test_list_no_normalizers(self):
+        """
+        List normalizers returns empty list.
+        """
+        expected = []
+        actual = list_normalizers({})
+        self.assertListEqual(actual, expected)
+
+
+class TestRegisterNormalizers(unittest.TestCase):
+    """
+    Test the register_normalizer function.
+    """
+
+    def test_register_normalizer(self):
+        """
+        Register a normalizer.
+        """
+        # Doesn't actually need to be a class.
+        actual = {}
+        register_normalizer(True, 'test', register=actual)
+        self.assertDictEqual(actual, {'test': True})
