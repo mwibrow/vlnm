4bbcd9cb015b9f758f4b033966abb226059c6fc7
Mark Wibrow <m.wibrow@gmail.com>
2018-12-06 14:17:28 +0000

    Start moving stuff to styles module

--- vlnm/plotting/style.py
+++ vlnm/plotting/style.py
@@ -11,6 +11,13 @@ in _categorical_ data (e.g., vowel category, reported gender, or particiant id).
 
 from typing import Any, Dict, List, Tuple
 
+import matplotlib.colors
+
+Color = Union[str, Tuple[float, float, float, float]]
+Colormap = Dict(any, Color)
+Colors = Union[List[Color], matplotlib.colors.ListedColormap]
+
+
 STYLES = dict(
     default=dict(
         color='black',
@@ -61,7 +68,61 @@ def get_group_styles(
                         value, STYLES['default'].get(prop))
         except KeyError:
             pass
-    # for prop in STYLES['default']:
-    #     if not prop in style:
-    #         style[prop] = STYLES['default'][prop]
+    for prop in STYLES['default']:
+        if not prop in style:
+            style[prop] = STYLES['default'][prop]
     return style
+
+def get_color_list(colors: Colors, levels: int = 1) -> List[Color]:
+    """Generate a list of colors.
+
+    colors:
+        The source of the color list.
+        - :obj:`str` name of a color map
+        - :obj:`str` name of a color
+        - :obj:`tuple` of :obj:`float` repesenting an RGBA color
+        - :obj:`list` a list of colors
+    levels:
+        The number of levels in the color list.
+    """
+    if isinstance(colors, list):
+        return colors[:levels]
+    elif isinstance(colors, matplotlib.colors.Colormap):
+        cmap = colors
+        if len(cmap.colors) != levels:
+            cmap = get_cmap(colors.name, lut=levels)
+        return list(cmap.colors)
+    else:
+        try:
+            cmap = get_cmap(colors, lut=levels)
+            if len(cmap.colors) != levels:
+                cmap = get_cmap(cmap.name, lut=levels)
+            return list(cmap.colors)
+        except ValueError:
+            return [colors]
+
+def get_color_map(
+        colors: Colors, categories: List[str]) -> Dict[str, Color]:
+    """Get a category-color mapping.
+
+    Parameters
+    ----------
+    colors:
+        Color specification.
+    categories:
+        A list of (vowel) categories.
+
+    Returns
+    -------
+    :
+        A dictionary mapping category labels to colors.
+    """
+    if isinstance(colors, dict):
+        return colors
+    color_list = get_color_list(colors, len(categories))
+    if not color_list:
+        color_list = [None for _ in categories]
+    color_map = {}
+    for i, category in enumerate(categories):
+        color_map[category] = color_list[i % len(color_list)]
+    return color_map
