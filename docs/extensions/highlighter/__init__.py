f24680bd0238b88148b14c96f2cb79419a7437ee
Mark Wibrow <m.wibrow@gmail.com>
2019-02-25 11:47:41 +0000

    Clean up after highlighter

--- docs/extensions/highlighter/__init__.py
+++ docs/extensions/highlighter/__init__.py
@@ -1,6 +1,7 @@
 """
 Extension providing custom lexers.
 """
+import os
 import setuptools
 
 from pygments.lexer import combined, include, bygroups, using, default, words
@@ -75,13 +76,29 @@ py3ext = extensions.highlighter:Python3LexerExtended
 """
 
 
+class CleanCommand(setuptools.Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):  # pylint: disable = no-self-use
+        os.system('rm -vrf ./dist ./highlighter.egg-info')
+
+
 def setup(_app):
     setuptools.setup(
         script_name='setup.py',
-        script_args=['install'],
+        script_args=['install', 'clean', '--all', 'deepclean'],
         name='highlighter',
         version='0.1',
         description=__doc__,
         author='Mark Wibrow',
         packages=['extensions.highlighter'],
-        entry_points=ENTRY_POINTS)
+        entry_points=ENTRY_POINTS,
+        cmdclass={
+            'deepclean': CleanCommand,
+        })
