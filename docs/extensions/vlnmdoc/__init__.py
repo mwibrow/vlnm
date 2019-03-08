3e93be466f0470296328f82c455436e59bed73b8
Mark Wibrow <m.wibrow@gmail.com>
2019-03-08 14:24:33 +0000

    Proof-of-concept reparser

--- docs/extensions/vlnmdoc/__init__.py
+++ docs/extensions/vlnmdoc/__init__.py
@@ -3,7 +3,11 @@ Some VLNM-specific stuff.
 """
 
 import docutils.nodes
-from docutils.parsers.rst import directives, Directive
+import docutils.utils
+from docutils.parsers.rst import directives, Directive, languages
+from docutils.parsers.rst.states import NestedStateMachine, Inliner, Struct
+
+from sphinx.parsers import RSTParser
 
 import vlnm
 from vlnm import get_normalizer, list_normalizers
@@ -18,31 +22,25 @@ class NormalizersDirective(Directive):
     has_content = False
 
     def run(self):
+        document = self.state.document
+        language = languages.get_language(document.settings.language_code)
+
         normalizers = list_normalizers()
         parent = docutils.nodes.paragraph()
+
+        string_name = ''
         for name in normalizers:
-            string_name = f"{name}"
-            node = docutils.nodes.paragraph(string_name, string_name, classes=['monospace'])
-            parent += node
+            string_name += f" ``{name}``, "
+
+        tmpdoc = docutils.utils.new_document('', document.settings)
+        parser = RSTParser()
+        parser.parse(string_name, tmpdoc)
+        parent += tmpdoc.children[0].children
         return [parent]
-
-
-class MonospaceRole:
-    """
-        Class for processing the :rst:role:`tt`.
-    """
-
-    def __call__(self, typ, rawtext, text, lineno, inliner,
-                 options=None, content=None):
-
-        node = docutils.nodes.Element(rawtext, classes=['monospace'])
-
-        return [node], []
 
 
 def setup(app):
     """
     Set up the sphinx extension.
     """
-    app.add_role('tt', MonospaceRole())
     app.add_directive('normalizers', NormalizersDirective)
