f14f9f74b895197881cc15fb1bb26aa0a94a1746
Mark Wibrow <m.wibrow@gmail.com>
2019-09-19 15:49:38 +0100

    More work on actions

--- vlnm/plotting/actions.py
+++ vlnm/plotting/actions.py
@@ -1,8 +1,13 @@
+"""
+Convenience functions
+"""
+
 from contextlib import contextmanager
 from functools import wraps
 
+import pandas as pd
 
-from .plots import get_plot
+from .plots import VowelPlot, get_plot
 
 
 def vowel_plot(func):
@@ -10,27 +15,54 @@ def vowel_plot(func):
     def wrapper(*args, plot=None, **kwargs):
         plot = plot or get_plot()
         if not plot:
-            raise ValueError('No plot')
-        return func(plot, *args, **kwargs)
+            raise ValueError('No plot is currently active.')
+        return func(_plot=plot, *args, **kwargs)
     return wrapper
 
 
 @vowel_plot
-def markers(plot, *args, **kwargs):
+def markers(*args, **kwargs):
+    """Add markers to an existing plot.
+
+    Parameters
+    ----------
+
+    See the documentation for the :method:`VowelPlot.markers` method.
+
+    Example
+    -------
+
+    ipython::
+
+        with VowelPlot():
+            with data(data=df, x='f2', y='f1'):
+                markers(color_by='vowel', color='tab20')
+
+
+    """
+    plot = kwargs.pop('_plot')
     plot.markers(*args, **kwargs)
 
 
 @vowel_plot
-def labels(plot, *args, **kwargs):
+def labels(*args, **kwargs):
+    plot = kwargs.pop('_plot')
     plot.labels(*args, **kwargs)
 
 
 @vowel_plot
+def ellipses(*args, **kwargs):
+    plot = kwargs.pop('_plot')
+    plot.ellipses(*args, **kwargs)
+
+
+@vowel_plot
 @contextmanager
-def legend(plot, **kwargs):
+def legend(**kwargs):
+    plot = kwargs.pop('_plot')
     legend_options = plot.legend_options
     try:
-        plot.legend_options = kwargs
+        plot.legend_options.update(**kwargs)
         yield plot
     finally:
         plot.legend_options = legend_options
@@ -38,16 +70,22 @@ def legend(plot, **kwargs):
 
 @vowel_plot
 @contextmanager
-def data(plot, data=None, x=None, y=None):
-    context_data = plot.plot_context.get('data')
-    context_x = plot.plot_context.get('x')
-    context_y = plot.plot_context.get('y')
+def data(
+        data: Union[str, pd.DataFrame, File] = None,
+        x: str = 'f2',
+        y: str = 'f1', **kwargs):
+    plot = kwargs.pop('_plot')
+    context = dict(
+        data=plot.plot_context.get('data'),
+        x=plot.plot_context.get('x'),
+        y=plot.plot_context.get('y'),
+    )
     try:
-        plot.plot_context['data'] = data
-        plot.plot_context['x'] = x
-        plot.plot_context['y'] = y
+        plot.plot_context.update(
+            data=data,
+            x=x,
+            y=y,
+        )
         yield plot
     finally:
-        plot.plot_context['data'] = context_data
-        plot.plot_context['x'] = context_x
-        plot.plot_context['y'] = context_y
+        plot.plot_context.update(**context)
