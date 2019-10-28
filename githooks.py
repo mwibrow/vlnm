4ba77076e0ec363e293dc43d87792e3450e2ddd3
Mark Wibrow <m.wibrow@gmail.com>
2019-10-28 16:32:51 +0000

    Completely unncessary updates to githooks

--- githooks.py
+++ githooks.py
@@ -14,30 +14,54 @@ REPO_PATH = os.path.abspath(os.path.dirname(__file__))
 colorama_init()
 
 
-def message(text, styles=None):
-    print(''.join(style or '' for style in styles) + text + Style.RESET_ALL)
+class Messenger:
+
+    tab = 0
+    tab_width = 4
+
+    def __enter__(self):
+        Messenger.tab += 1
+        return self
+
+    def __exit__(self, *args):
+        Messenger.tab -= 1
+
+    @classmethod
+    def message(cls, text, styles=None):
+        print(' ' * (cls.tab * cls.tab_width) +
+              ''.join(style or '' for style in styles) + text + Style.RESET_ALL)
+
+    def __call__(self, *args, **kwargs):
+        self.message(*args, **kwargs)
+
+    def indent(self):
+        return self
 
 
 def pre_commit():
+    message = Messenger()
     message('Running pre-commit...', [Fore.YELLOW])
-    message('   - Checking changed files', [Fore.YELLOW])
-    repo = Repo(REPO_PATH)
-    diff = repo.git.diff(repo.active_branch, name_only=True)
-    changed = [
-        item for item in diff.split('\n') if item.endswith('.py')]
-    message('   - {} found'.format(len(changed)), [Fore.YELLOW])
-    if changed:
-        message('   - Running autopep8 on files', [Fore.YELLOW])
-        options = autopep8.parse_args([
-            '--global-config=.pep8',
-            '--in-place'] + changed)
-        autopep8.fix_multiple_files(changed, options)
+    with message.indent():
+        message('- Checking changed files', [Fore.YELLOW])
+        repo = Repo(REPO_PATH)
+        diff = repo.git.diff(repo.active_branch, name_only=True)
+        changed = [
+            item for item in diff.split('\n') if item.endswith('.py')]
+        message('- {} found'.format(len(changed)), [Fore.YELLOW])
+        if changed:
+            message('- Running autopep8 on files', [Fore.YELLOW])
+            options = autopep8.parse_args([
+                '--global-config=.pep8',
+                '--in-place'] + changed)
+            autopep8.fix_multiple_files(changed, options)
     message('...done.', [Fore.YELLOW])
     return True
 
 
 HOOKS = {
     'pre-commit': pre_commit
+
+
 }
 
 
