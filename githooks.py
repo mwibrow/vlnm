eaffd714dec90265ce7e52e0c2ab1c2832bb1e7f
Mark Wibrow <m.wibrow@gmail.com>
2019-10-28 10:31:57 +0000

    Update git hooks script

--- githooks.py
+++ githooks.py
@@ -5,22 +5,34 @@ import os
 import sys
 
 import autopep8
+from colorama import init as colorama_init, Fore, Style
 from git import Repo
+
 
 REPO_PATH = os.path.abspath(os.path.dirname(__file__))
 
+colorama_init()
+
+
+def message(text, styles=None):
+    print(''.join(style or '' for style in styles) + text + Style.RESET_ALL)
+
 
 def pre_commit():
+    message('Running pre-commit...', [Fore.YELLOW])
+    message('   Checking changed files...', [Fore.YELLOW])
     repo = Repo(REPO_PATH)
     diff = repo.git.diff(repo.active_branch, name_only=True)
     changed = [
         item for item in diff.split('\n') if item.endswith('.py')]
-
-    options = autopep8.parse_args([
-        '--global-config=.pep8',
-        '--in-place'] + changed)
-    autopep8.fix_multiple_files(changed, options)
-
+    message('       {} found'.format(len(changed)), [Fore.YELLOW])
+    if changed:
+        message('   Running autopep8', [Fore.YELLOW])
+        options = autopep8.parse_args([
+            '--global-config=.pep8',
+            '--in-place'] + changed)
+        autopep8.fix_multiple_files(changed, options)
+    message('done', [Fore.YELLOW])
     return True
 
 
